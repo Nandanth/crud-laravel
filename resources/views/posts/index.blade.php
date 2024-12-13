4d945a6dff9195647fb116f53f22124186441289
nandanth <nandanth19@gmail.com>
2024-12-13 19:25:06 +0700

    update all code

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,15 @@
+@extends('layout.main')
+@section('container')
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Posts - STEKOM</title>
+    <title>Data Posts</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 </head>
@@ -72,6 +77,8 @@
             
         @endif
     </script>
+
+    @endsection
 
 </body>
 </html>
